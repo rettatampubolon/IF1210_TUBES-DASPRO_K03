9b8f3eecdbdb7b86790ce2f815339356d306ef34
rettatampubolon <81466524+rettatampubolon@users.noreply.github.com>
2023-04-20 15:20:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -73,3 +73,23 @@ def login():
 
                 else:
                     print(f'Username “{username}” sudah diambil!')
+
+#F04 
+def hilangkan_jin(user_matrix):
+    username = input("Masukkan username jin : ")
+    indeks = -1
+    for i in range (length(user_matrix)):
+        if user_matrix[i][0] == username:
+            print(f"Apakah anda yakin ingin menghapus jin dengan username {username} (Y/N)?")
+            yn = input()
+
+            if yn == 'Y' or yn == 'y':
+                indeks = i
+                print("Jin telah berhasil dihapus dari alam gaib.")
+                break
+            else:
+                break
+        elif i == length(user_matrix) - 1:
+            print("Tidak ada jin dengan username tersebut.")
+        else:
+            indeks += 1
