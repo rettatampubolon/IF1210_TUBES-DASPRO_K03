62113fcf5a5719d1cf841b34d53a435bd7951a17
rettatampubolon <81466524+rettatampubolon@users.noreply.github.com>
2023-04-20 10:11:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,87 @@
 #TUGAS BESAR DASAR PEMROGRAMAN K03
+
+import csv
+
+#Fungsi Length
+def length(string):
+    #Inisiasi jumlah kata/matriks
+    count = 0
+    
+    #Loop
+    for i in string:
+        count = count + 1
+    return count
+
+
+#f01 - fungsi log in
+def login():
+    username = input("Username: ")
+    password = input("Password: ")
+
+    with open("user.csv", "r") as func:
+        reader = csv.reader(func, delimiter=";")
+        for row in reader:
+            if row[0] == username and row[1] == password:
+                print(f"Selamat datang, {row[2]}! \n Masukkan command “help” untuk daftar command yang dapat kamu panggil.")
+                return True
+            elif username != row [0] and row[1] == password:
+                print("Username tidak terdaftar!")
+                return False
+            elif username == row [0] and row[1] != password:
+                print ("Password salah!")
+                return False
+    print(f"Login gagal! \n Anda telah login dengan {username}, silahkan lakukan “logout” sebelum melakukan login kembali.")
+    return False
+
+  #f02 -
+  
+  #f03 - Summon Jin
+  def summonjin():
+#I.S. 
+#F.S.
+
+    global JumlahJin
+
+    if (JumlahJin) >= 100:
+        print("Jumlah jin telah mencapai maksimum.")
+    else:
+        print("Jenis jin yang dapat dipanggil: ")
+        print("(1) Pengumpul - Bertugas mengumpulkan bahan bangunan")
+        print("(2) Pembangun - Bertugas membangun candi")
+
+        jenis_jin = int(input("Masukkan nomor jenis jin yang ingin dipanggil:"))
+
+        if jenis_jin != 1 or jenis_jin !=2:
+            print(f"Tidak ada jenis jin bernomor {jenis_jin}")
+        else:
+            print(f'Memilih jin "{jenis_jin}"')
+            username = input("Masukkan username jin: ")
+            password = input("Masukkan password jin: ")
+
+            if length(password) < 5 and length(password) > 25:
+                print("Password panjangnya harus 5-25 karakter!")
+                password = input("Masukkan password jin: ")
+            else:
+                if jenis_jin == 1:
+                    while length(password) >= 5 and length(password) <=25:
+                        user = [username, password, "Pengumpul"]
+                        print("Mengumpulkan sesajen...")
+                        print("Menyerahkan sesajen...")
+                        print("Membacakan mantra...")
+                        print()
+                        print(f"Jin, {username} berhasil dipanggil")
+                        break
+                else:
+                    print(f'Username “{username}” sudah diambil!')
+                
+                if jenis_jin == 2:
+                    while length(password) >= 5 and length(password) <=25:
+                        user = [username, password, "Pembangun"]
+                        print("Mengumpulkan sesajen...")
+                        print("Menyerahkan sesajen...")
+                        print("Membacakan mantra...")
+                        print()
+                        print(f"Jin, {username} berhasil dipanggil")
+                        break
+                else:
+                    print(f'Username “{username}” sudah diambil!')
