b5b7a8c75a8df87fae8adfa3cf1ac4346bd264de
Farhannr28 <80628849+Farhannr28@users.noreply.github.com>
2023-04-30 18:17:20 +0700

    Update f07.py

--- f07.py
+++ f07.py
@@ -1,25 +1,14 @@
-import csv
-import random
+import b01
 
 def jin_pengumpul():
-    file = open('file-eksternal/bahan_bangunan.csv', 'r')
-    file_reader = csv.reader(file, delimiter=';')
-    data_bahan = [row for row in file_reader]
-
-    pasir = random.randint(0,5)     # random number untuk bahan pasir 
-    batu = random.randint(0,5)      # random number untuk bahan batu
-    air = random.randint(0,5)       # random number untuk bahan air
+    pasir = b01.randomize("kumpul")    # random number untuk bahan pasir 
+    batu = b01.randomize("kumpul")      # random number untuk bahan batu
+    air = b01.randomize("kumpul")       # random number untuk bahan air
     bahan = [pasir,batu,air]
-    data_bahan[1][2] = int(data_bahan[1][2]) + pasir
-    data_bahan[2][2] = int(data_bahan[2][2]) + batu
-    data_bahan[3][2] = int(data_bahan[3][2]) + air
+    array_bahan[1][2] = int(array_bahan[1][2]) + pasir
+    array_bahan[2][2] = int(array_bahan[2][2]) + batu
+    array_bahan[3][2] = int(array_bahan[3][2]) + air
     
     print(f"Jin menemukan {pasir} pasir, {batu} batu, dan {air} air.")
-    
-    file = open('file-eksternal/bahan_bangunan.csv', 'w', newline='')
-    file_writer = csv.writer(file, delimiter=';')
-    file_writer.writerows(data_bahan)
-    file.close()
-
     return bahan
 jin_pengumpul()
