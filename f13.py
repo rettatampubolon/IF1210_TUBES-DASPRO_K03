ae28da126df874e87e1fc44a51f09a760dbdfa26
liannagrace <117985072+liannagrace@users.noreply.github.com>
2023-04-30 21:06:45 +0700

    Update f13.py

--- f13.py
+++ f13.py
@@ -2,27 +2,27 @@ import os
 import argparse
 import util
 import sys
+import commands
 
-argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument("--nama_folder")
-args = argument_parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('nama_folder', nargs='?')
+args = parser.parse_args()
 directory = args.nama_folder
-
 
 list_directory = os.listdir('.')
 print()
 
 save = False
-for i in range(util.length(list_directory)):
+for i in range(util.length(list_directory)): #memeriksa folder save di current path
     if 'save' == list_directory[i]:
         save = True
         break
 
 if save:
-    list_save = os.listdirectory('./save')
+    list_save = os.listdir('./save')
     idx = 0
 
-for i in range(util.length(list_directory)):
+for i in range(util.length(list_directory)): #memeriksa nama folder yang diinput
     if folder_directory == list_directory[i]:
         print('Loading...')
         print('Selamat datang di program “Manajerial Candi”')
@@ -32,10 +32,15 @@ for i in range(util.length(list_directory)):
         if folder_directory == {f'save/list_save[idx]'}:
             print('Loading...')
             print('Selamat datang di program "Manajerial Candi"')
-            print('Silakan masukkan username Anda')
+            print('Untuk melihat daftar command yang dapat dipanggil, silakan ketik "help" ')
             break
         idx =+ 1
     else:
-        if folder_directory:
+        if folder_directory: #memeriksa apakah ada masukan argumen dari user
             print(f'Folder"(folder_directory)" tidak ada')
             sys.exit()
+         else: #tidak ada masukan argumen dari user
+            print('tidak ada masukan argumen dari user')
+            print('usage: python main.py <nama_folder>')
+            sys.exit()
+            
