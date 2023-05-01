2066070c58d1fc8b8093508c3349678fbfeff0c5
Farhannr28 <80628849+Farhannr28@users.noreply.github.com>
2023-05-02 00:37:19 +0700

    Lolos Testing
    Error fix di kedua fungsi

--- spesifikasi/f08.py
+++ spesifikasi/f08.py
@@ -1,45 +1,36 @@
-import csv
+from spesifikasi import b01
 import util
-import random
-import f07
-
-file = open('file-eksternal/user.csv', 'r')
-file_reader = csv.reader(file, delimiter=';')
-user = [row for row in file_reader]
-
-file = open('file-eksternal/bahan_bangunan.csv', 'r')
-file_reader = csv.reader(file, delimiter=';')
-bahan = [row for row in file_reader]
 
 def batch_kumpul(user_matrix: list[list], bahan_matrix: list[list]):
     count = 0
+    bahan1 = bahan_matrix[1][2] 
+    bahan2 = bahan_matrix[2][2]  
+    bahan3 = bahan_matrix[3][2] 
     for i in range(util.length(user_matrix)):
-        if user_matrix[i][2] == "jin_pengumpul":
-            bahan1 = f07.jin_pengumpul()
+        if user_matrix[i][2] == "Pengumpul":
+            pasir = b01.randomize("kumpul")    # random number untuk bahan pasir 
+            batu = b01.randomize("kumpul")      # random number untuk bahan batu
+            air = b01.randomize("kumpul")       # random number untuk bahan air
+            bahan = [pasir,batu,air]
+            util.array_bahan[1][2] = int(util.array_bahan[1][2]) + pasir
+            util.array_bahan[2][2] = int(util.array_bahan[2][2]) + batu
+            util.array_bahan[3][2] = int(util.array_bahan[3][2]) + air
             count = count + 1
     if count > 0:
-        bahan_matrix[1][2] = int(bahan_matrix[1][2]) + bahan1[0]
-        bahan_matrix[2][2] = int(bahan_matrix[2][2]) + bahan1[1]
-        bahan_matrix[3][2] = int(bahan_matrix[3][2]) + bahan1[2]
-        
         print(f"Mengerahkan {count} jin untuk mengumpulkan bahan.")
-        print(f"Jin menemukan {bahan1[0]} pasir, {bahan1[1]} batu, dan {bahan1[2]} air.")
-
-        file = open('file-eksternal/bahan_bangunan.csv', 'w', newline='')
-        file_writer = csv.writer(file, delimiter=';')
-        file_writer.writerows(bahan_matrix)
-        file.close()
+        print(f"Jin menemukan {bahan_matrix[1][2] - bahan1} pasir, {bahan_matrix[2][2] - bahan2} batu, dan {bahan_matrix[3][2] - bahan3} air.")
     else:
         print("Kumpul gagal. Anda tidak punya jin pengumpul. Silahkan summon terlebih dahulu.")
 # batch_kumpul(user,bahan)
 
-def batch_bangun(user_matrix,array_bahan):     
+
+def batch_bangun(user_matrix,array_bahan,candi):     
     count = 0
     bahan2 = [0 for i in range(3)]
     for i in range(util.length(user_matrix)):
-        if user_matrix[i][2] == "jin_pembangun":
+        if user_matrix[i][2] == "Pembangun":
             for i in range(3):
-                bahan2[i] = bahan2[i] + random.randint(1,5)
+                bahan2[i] = bahan2[i] + b01.randomize("bangun")
             count = count + 1
     if count > 0:
         print(f"Mengerahkan {count} jin untuk membangun candi dengan total bahan {bahan2[0]} pasir, {bahan2[1]} batu, dan {bahan2[2]} air.")
@@ -51,23 +42,14 @@ def batch_bangun(user_matrix,array_bahan):
             pasir = bahan2[0]
             batu = bahan2[1]
             air = bahan2[2]
-
-            file = open('file-eksternal/bahan_bangunan.csv', 'w', newline='')
-            file_writer = csv.writer(file, delimiter=';')
-            file_writer.writerows(array_bahan)
-                
-            idx = 0
             username = ''
             for i in range(util.length(user_matrix)):
-                if user_matrix[i][2] == "jin_pembangun":
-                    idx = idx + 1
+                if user_matrix[i][2] == "Pembangun":
+                    idx=1
+                    while(candi[idx][0] != None):
+                        idx += 1
                     username = user_matrix[i][0]
-                    newline = [idx,username,pasir,batu,air]
-
-                    file = open('file-eksternal/candi.csv', 'a', newline='')
-                    file_writer = csv.writer(file, delimiter=';')
-                    file_writer.writerow(newline)
-                    file.close()
+                    candi[idx] = [idx,username,pasir,batu,air]
         else:
             kurang = [0 for i in range(3)]
             for i in range(3):
