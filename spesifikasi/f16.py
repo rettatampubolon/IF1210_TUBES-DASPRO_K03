f3ce58ce1ac6266a9aa109c4c71221a7fa66c4c1
Farhannr28 <80628849+Farhannr28@users.noreply.github.com>
2023-05-02 03:52:05 +0700

    Lolos Testing

--- spesifikasi/f16.py
+++ spesifikasi/f16.py
@@ -1,13 +1,16 @@
-print(">>> exit")
+from spesifikasi import f14
+
 def PeriksaJawaban(Answer):
     if Answer == 'y' or Answer == 'n': #jika inputan yang dimasukkan user benar (y/n)
         return True
     else:
         return False
 
-Answer = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah?")
+def exit():
+    Answer = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah?")
 
-while PeriksaJawaban(Answer) == False:
-    Answer = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah?") #menanyakan kembali kepada user karena input yang dimasukkan tidak sesuai
-    if PeriksaJawaban(Answer) == True:
-        break
+    while PeriksaJawaban(Answer) == False:
+        Answer = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah?") #menanyakan kembali kepada user karena input yang dimasukkan tidak sesuai
+        if PeriksaJawaban(Answer) == True:
+            f14.save()
+            break
