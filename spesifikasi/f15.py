e231698c79bb392d539ebc0a5f15fdb6c4c945a4
liannagrace <117985072+liannagrace@users.noreply.github.com>
2023-05-01 19:43:39 +0700

    Update f15.py

--- spesifikasi/f15.py
+++ spesifikasi/f15.py
@@ -1,13 +1,13 @@
-f01 import * #untuk memanggil f01-login
+f01 import *
 
-def help(info_user):
+def help(role):
     print('=========== HELP ===========')
-    if info_user[0] == False: #Pemain yang belum login
+    if role == False: #Pemain yang belum login atau input yang dimasukkan tidak valid
         print('1. login')
         print('untuk masuk menggunakan akun')
         print('2. exit')
         print('untuk keluar dari program dan kembali ke terminal')
-    elif info_user[3] == 'bandung_bondowoso':
+    elif role == 'bandung_bondowoso':
         print('1. logout')
         print('untuk keluar dari akun yang digunakan sekarang')
         print('2. summonjin')
@@ -26,7 +26,7 @@ def help(info_user):
         print('untuk mengambil laporan candi guna mengetahui progress pembangunan candi')
         print('9. save')
         print('untuk menyimpan data user')
-    elif info_user[3] == 'roro_jongrang':
+    elif role == 'roro_jongrang':
         print('1. logout')
         print('untuk keluar dari akun yang digunakan sekarang')
         print('2. hancurkancandi')
@@ -35,20 +35,19 @@ def help(info_user):
         print('untuk menyelesaikan permainan dengan memalsukan pagi hari')
         print('4. save')
         print('untuk menyimpan data user')
-    elif info_user[3] == 'jin_pengumpul':
+    elif role == 'jin_pengumpul':
         print('1. logout')
         print('untuk keluar dari akun yang digunakan sekarang')
         print('2. kumpul')
         print('untuk mengumpulkan bahan-bahan yang diperlukan untuk membangun candi')
         print('3. save')
         print('untuk menyimpan data user')
-    elif info_user[3] == 'jin_pembangun':
+    elif role == 'jin_pembangun':
         print('1. logout')
         print('untuk keluar dari akun yang digunakan sekarang')
         print('2. bangun')
         print('untuk membangun candi menggunakan bahan bangunan yang ada')
         print('3. save')
         print('untuk menyimpan data user')
-    
 
 
