38b26c0f1df55ab0b59c6593348970c5c8de3c97
rettatampubolon <81466524+rettatampubolon@users.noreply.github.com>
2023-05-01 09:40:36 +0700

    main.py

--- main.py
+++ main.py
@@ -51,10 +51,10 @@ def login():
 def logout(login):
     if not login:
         print("Anda belum login.")
+        print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
         return False
     else:
-        print("Logout gagal!")
-        print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
+        print("Berhasil logout!")
         login = False
         return True
   
