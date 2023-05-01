77a2cf0a48850731c4d51499949b95c9df766ae9
Farhannr28 <80628849+Farhannr28@users.noreply.github.com>
2023-05-02 03:46:20 +0700

    Update f01.py

--- spesifikasi/f01.py
+++ spesifikasi/f01.py
@@ -4,13 +4,18 @@ def login():
     if util.user == "":
         username = input("Username: ")
         password = input("Password: ")
+        found = False
         for row in util.array_user:
             if row[0] == username and row[1] == password:
-                print(f"Selamat datang, {row[2]}! \nMasukkan command “help” untuk daftar command yang dapat kamu panggil.")
+                print(f"Selamat datang, {row[2]}! \n Masukkan command “help” untuk daftar command yang dapat kamu panggil.")
                 util.user = username
-            elif username != row [0] and row[1] == password:
-                print("Username tidak terdaftar!")
+                found = True
+                break
             elif username == row [0] and row[1] != password:
                 print ("Password salah!")
+                break
+                found = True
+        if (not found):
+            print("Username tidak terdaftar!")
     else:
         print(f"Login gagal! \n Anda telah login dengan {util.user}, silahkan lakukan “logout” sebelum melakukan login kembali.")
