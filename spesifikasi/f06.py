c5dda21c7a753ec5f4413ab50cb00ee90db8df0e
Farhannr28 <80628849+Farhannr28@users.noreply.github.com>
2023-05-01 23:45:00 +0700

    Lolos Testing
    Sedikit perubahan di line 16-19 untuk mencari index candi yang masih kosong

--- spesifikasi/f06.py
+++ spesifikasi/f06.py
@@ -1,33 +1,29 @@
-import b01
+from spesifikasi import b01
 import util
 
-def jin_pembangun(user: list[list], bahan: list[list]):
-    global JumlahJin
-    id_candi = [0 for i in range(100)]
-    counting_candi = 100
+def jin_pembangun(user: list[list], bahan: list[list], candi: list[list]):
+    util.JumlahJin
     idx = 0
     username = ''
     for i in range(util.length(user)):
-        if user[i][2] == "jin_pembangun":
+        if user[i][2] == "Pembangun":
             username = user[i][0]
-
     pasir =b01.randomize("bangun")
     batu = b01.randomize("bangun")
     air = b01.randomize("bangun")
     print(f"Men-generate bahan bangunan ({pasir} pasir, {batu} batu, dan {air} air.)")
     if int(bahan[1][2]) >= pasir and int(bahan[2][2]) >= batu and int(bahan[3][2]) >= air:
-        for i in range(100):
-            if id_candi[i] == 0:
-                id_candi[i] = id_candi[i] + i + 1
-                idx = id_candi[i]
+        for i in range(util.length(candi)):
+            if (candi[i][0] == None):
+                idx=i
                 break
         print("Candi berhasil dibangun.")
-        counting_candi = counting_candi - 1
-        print(f"Sisa candi yang harus dibangun: {counting_candi}")
+        util.counting_candi = util.counting_candi - 1
+        print(f"Sisa candi yang harus dibangun: {util.counting_candi}")
         bahan[1][2] = int(bahan[1][2]) - pasir
         bahan[2][2] = int(bahan[2][2]) - batu
         bahan[3][2] = int(bahan[3][2]) - air
-        array_candi[idx]=[idx, array_jin[JumlahJin+2][0], pasir, batu, air]
+        candi[idx]=[idx, username, pasir, batu, air]
     else:
         print("Bahan bangunan tidak mencukupi.")
         print("Candi tidak bisa dibangun!")
