6e3ed4bf60ca0e63dc3f3a76f261198d13eb1ade
Farhannr28 <80628849+Farhannr28@users.noreply.github.com>
2023-05-02 03:41:59 +0700

    Lolos Testing

--- spesifikasi/f15.py
+++ spesifikasi/f15.py
@@ -1,13 +1,13 @@
-f01 import *
+from spesifikasi import f01
 
-def help(role):
+def help(info_user):
     print('=========== HELP ===========')
-    if role == False: #Pemain yang belum login atau input yang dimasukkan tidak valid
+    if info_user == "": #Pemain yang belum login
         print('1. login')
         print('untuk masuk menggunakan akun')
         print('2. exit')
         print('untuk keluar dari program dan kembali ke terminal')
-    elif role == 'bandung_bondowoso':
+    elif info_user == "bandung_bondowoso":
         print('1. logout')
         print('untuk keluar dari akun yang digunakan sekarang')
         print('2. summonjin')
@@ -26,7 +26,7 @@ def help(role):
         print('untuk mengambil laporan candi guna mengetahui progress pembangunan candi')
         print('9. save')
         print('untuk menyimpan data user')
-    elif role == 'roro_jongrang':
+    elif info_user == "roro_jonggrang":
         print('1. logout')
         print('untuk keluar dari akun yang digunakan sekarang')
         print('2. hancurkancandi')
@@ -35,19 +35,17 @@ def help(role):
         print('untuk menyelesaikan permainan dengan memalsukan pagi hari')
         print('4. save')
         print('untuk menyimpan data user')
-    elif role == 'jin_pengumpul':
+    elif info_user == "Pengumpul":
         print('1. logout')
         print('untuk keluar dari akun yang digunakan sekarang')
         print('2. kumpul')
         print('untuk mengumpulkan bahan-bahan yang diperlukan untuk membangun candi')
         print('3. save')
         print('untuk menyimpan data user')
-    elif role == 'jin_pembangun':
+    elif info_user == "Pembangun":
         print('1. logout')
         print('untuk keluar dari akun yang digunakan sekarang')
         print('2. bangun')
         print('untuk membangun candi menggunakan bahan bangunan yang ada')
         print('3. save')
         print('untuk menyimpan data user')
-
-
