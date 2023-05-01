c612006cf3a5c2b8be5d68e7fafbfcc8ba162ab6
Farhannr28 <80628849+Farhannr28@users.noreply.github.com>
2023-05-01 23:49:54 +0700

    Lolos Testing

--- spesifikasi/f07.py
+++ spesifikasi/f07.py
@@ -1,12 +1,13 @@
-import b01
+from spesifikasi import b01
+import util
 
 def jin_pengumpul():
     pasir = b01.randomize("kumpul")    # random number untuk bahan pasir 
     batu = b01.randomize("kumpul")      # random number untuk bahan batu
     air = b01.randomize("kumpul")       # random number untuk bahan air
     bahan = [pasir,batu,air]
-    array_bahan[1][2] = int(array_bahan[1][2]) + pasir
-    array_bahan[2][2] = int(array_bahan[2][2]) + batu
-    array_bahan[3][2] = int(array_bahan[3][2]) + air
+    util.array_bahan[1][2] = int(util.array_bahan[1][2]) + pasir
+    util.array_bahan[2][2] = int(util.array_bahan[2][2]) + batu
+    util.array_bahan[3][2] = int(util.array_bahan[3][2]) + air
     
     print(f"Jin menemukan {pasir} pasir, {batu} batu, dan {air} air.")
